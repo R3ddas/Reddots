ec0bf36d8e85364dc4c105055eb148aa055d883d
R3ddas <144235431+R3ddas@users.noreply.github.com>
2026-08-25 17:54:40 +0200

    Fix command execution in hyprland.lua

--- hyprland.lua
+++ hyprland.lua
@@ -36,7 +36,7 @@ local menu        = "hyprlauncher"
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function ()
-hl.exec_cmd(quickshell)
+    hl.exec_cmd("quickshell")
 --   hl.exec_cmd("nm-applet")
 --   hl.exec_cmd("waybar & hyprpaper & firefox")
 end)
