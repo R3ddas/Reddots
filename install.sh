30743775579acfc320dbc85562199758ecc05a1f
R3ddas <144235431+R3ddas@users.noreply.github.com>
2026-08-25 21:13:09 +0200

    Remove dolphin installation and add echo statements

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ echo "Paquetes necesarios"
 sed 's/#.*//' packages.txt | grep -v '^\s*s' \
 | xargs -r sudo pacman -S --needed  --noconfirm
 
+echo "Paquetes no utilizados"
+
+sudo pacman -Rns dolphin # Quito dolphin porque instalo nemo
+
 echo "Sistema de archivos"
 
 mkdir -p ~/.config/quickshell
