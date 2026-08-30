82d487cd0b92f1eb3ccd197687be8e3e5fb0ce0f
R3ddas <jaimedoblelopez@gmail.com>
2026-08-30 18:43:38 +0200

    Limpiexa general

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -98,26 +98,26 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 14,
+        rounding       = 16,
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
-        active_opacity   = opacity, -- Opacidad de la ventana activa
-        inactive_opacity = opacity, -- Opacidad de las ventanas inactivas
-        fullscreen_opacity = opacity, -- Opacidad de las ventanas en pantalla completa
+        active_opacity   = opacity,     -- Opacidad de la ventana activa
+        inactive_opacity = opacity,     -- Opacidad de las ventanas inactivas
+        fullscreen_opacity = opacity,   -- Opacidad de las ventanas en pantalla completa
 
         shadow = {
             enabled      = true,
-          range        = 4,
-          render_power = 3,
-          color        = 0xee1a1a1a,
+            range        = 4,
+            render_power = 3,
+            color        = 0xee1a1a1a,
         },
 
         blur = {
             enabled   = true,
-          size      = 3,
-          passes    = 1,
-          vibrancy  = 0.1696,
+            size      = 3,
+            passes    = 1,
+            vibrancy  = 0.1696,
         },
     },
 
