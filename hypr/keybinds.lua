19602656049f326ecabb07feedafae13b206675f
R3ddas <jaimedoblelopez@gmail.com>
2026-09-03 21:07:54 +0200

    Intentado sin exito corregir la trasnparencia en pantalla completa

--- hypr/keybinds.lua
+++ hypr/keybinds.lua
@@ -1,18 +1,10 @@
----------------------
----- MY PROGRAMS ----
----------------------
--- Set programs that you use
---local terminal    = "alacritty"
---local fileManager = "nemo"
---local menu        = "hyprlauncher"
-
 local programs = require("programs")
 local terminal, fileManager, menu = programs.terminal, programs.fileManager, programs.menu
 
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(terminal))                       -- Abre un terminal
-local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close())   -- Cierra la ventana sobre la que esté el ratón (aqunue no haya pulsado)
+local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close())   -- Cierra la ventana sobre la que esté el ratón (aunque no haya pulsado)
 
 -- Move focus with mainMod + arrow keys
 hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
@@ -50,8 +42,8 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
-hl.bind("SUPER + I", hl.dsp.window.set_prop({ prop = "opaque", value = "toggle" }))         -- Activo/Desactivo la trasnparencia con Super + I
-hl.bind("SUPER + F", hl.dsp.window.fullscreen({ action = "toggle" }))                       -- Entro/salgo de pantalla completa con Super + F
+hl.bind("SUPER + I", hl.dsp.window.set_prop({ prop = "opaque", value = "toggle" }), { dont_inhibit = true })  -- Activo/Desactivo la trasnparencia con Super + I (dont_inhibit intenta (mal) arreglar el comportamiento en fullscreen)
+hl.bind("SUPER + F", hl.dsp.window.fullscreen({ action = "toggle" }))                                         -- Entro/salgo de pantalla completa con Super + F
 
 -- Al pulsar y soltar solo la tecla Super (sin combinar con otra), muestro/oculto el widget inferior
 hl.bind("SUPER + SUPER_L", hl.dsp.exec_cmd("qs ipc call launcher toggle"), { release = true })
